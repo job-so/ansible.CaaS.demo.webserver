555bd6c93ae1730c44a29f54a763381fab261ff0
Olivier GROSJEANNE <github@grosjeanne.name>
2017-09-19 11:14:31 +0200

    Update index.php

--- index.php
+++ index.php
@@ -3,11 +3,10 @@
     <center>
       <img width=240px src="http://www.mbda-systems.com/wp-content/uploads/2015/06/logo.svg" /><br />
       <h1><?php echo "SERVER : ".$_SERVER["SERVER_ADDR"]."/".$_SERVER["REMOTE_PORT"]."</h1>";?>
-      <form action="/msg">
-        <input type="text" name="msg" />
-        <input type="submit" />
-      </form>
+        <hr />
       <?php echo(file_get_contents("http://192.168.40.8:3000"));?>
+        <hr />
+      <img src="https://upload.wikimedia.org/wikipedia/fr/thumb/2/22/Dimension_Data_logo.jpg/260px-Dimension_Data_logo.jpg" />
     </center>
   </body>
 </html>
