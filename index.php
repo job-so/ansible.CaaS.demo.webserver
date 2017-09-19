6baaf00cd5e022a6c42bf2a5a8e2600fb301e4fd
Olivier GROSJEANNE <github@grosjeanne.name>
2017-09-19 11:08:58 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <html>
   <body>
     <center>
-      <img width=80px src="http://www.mbda-systems.com/wp-content/uploads/2015/06/logo.svg" /><br />
+      <img width=240px src="http://www.mbda-systems.com/wp-content/uploads/2015/06/logo.svg" /><br />
       <h1><?php echo "SERVER : ".$_SERVER["SERVER_ADDR"]."/".$_SERVER["REMOTE_PORT"]."</h1>";?>
       <form action="/msg">
         <input type="text" name="msg" />
